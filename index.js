dec56a34ee4c05807050d918820c2bd7ee13a158
Sorata Fujitsuka <57311258+KomattaSorata@users.noreply.github.com>
2020-01-31 20:25:26 +0800

    Minor fixes

--- index.js
+++ index.js
@@ -15,15 +15,20 @@ client.on('message', msg => {
 });
 
 client.on('message', msg => {
-  if(msg.author.bot === false){fetch_sourcelines(msg.content);}
+  if(msg.author.bot === false){
+    if(msg.content.includes("\n") || msg.content.includes("。") || msg.content.includes("「") || msg.content.includes( "」")){
+      // Prohibit function for potential long text, avoiding complex and low accuracy processing.
+    }else{
+      fetch_sourcelines(msg.content);}
+    }
 });
 
 client.setInterval( () => {
-    const srcwords = fs.readFileSync('srcwords.txt').toString().split("\n");
-    const word = srcwords[Math.floor(Math.random() * srcwords.length)];
-    const srclines = fs.readFileSync('srclines.txt').toString().split("\n");
-    const generatedMessage = srclines[Math.floor(Math.random() * srclines.length)].replace("${word}", word);
-    client.channels.get(mibunshou.channelid).send(generatedMessage)
+  const srcwords = fs.readFileSync('srcwords.txt').toString().split("\n");
+  const word = srcwords[Math.floor(Math.random() * srcwords.length)];
+  const srclines = fs.readFileSync('srclines.txt').toString().split("\n");
+  const generatedMessage = srclines[Math.floor(Math.random() * srclines.length)].replace("${word}", word);
+  client.channels.get(mibunshou.channelid).send(generatedMessage)
 }, 60000);
 
 client.login(mibunshou.discord_token);
